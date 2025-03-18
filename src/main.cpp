3a98ddec98c1818613539efcb7932808670211bf
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-03-18 16:26:46 +0700

    Add U8g2 library and implement OLED display for sensor readings

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,15 @@
 #include <ESP32Servo.h>
 #include <MQUnifiedsensor.h> // Thêm thư viện MQUnifiedsensor
 #include <ArduinoJson.h>
+#include <U8g2lib.h>
 
 // Định nghĩa chân
 #define DS18B20_PIN 22
-#define RELAY_PIN 23
+#define RELAY_PIN 13
 #define HUMIDITY_SENSOR_PIN 34
 #define SIM_RX 16
 #define SIM_TX 17
-#define SERVO_PIN 21
+#define SERVO_PIN 12
 #define MQ135_PIN 35 // Chân GPIO cho MQ135 (analog)
 #define ZH03B_RX 18  // Chân RX cho ZH03B
 #define ZH03B_TX 19  // Chân TX cho ZH03B
@@ -31,6 +32,7 @@ HardwareSerial simSerial(2);
 HardwareSerial zh03bSerial(1);
 Servo servo;
 MQUnifiedsensor MQ135("ESP32", 3.3, 12, MQ135_PIN, "MQ-135");
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
 // Biến toàn cục
 float ahtTemp = 0.0, ahtHumidity = 0.0, dsTemp = 0.0, capHumidity = 0.0, co2PPM = 0.0, pm25 = 0.0, pm10 = 0.0;
@@ -75,6 +77,21 @@ void sensorTask(void *pvParameters)
       pm10 = pm10Raw / 10.0; // Chuyển sang µg/m³
       xSemaphoreGive(dataMutex);
     }
+
+    // Hiển thị lên OLED
+    char buffer[32];
+    u8g2.clearBuffer(); // Xóa buffer
+
+    snprintf(buffer, sizeof(buffer), "T1:%.1fC H1:%.1f%%", ahtTemp, ahtHumidity);
+    u8g2.drawStr(0, 10, buffer); // Dòng 1: Nhiệt độ AHT20, độ ẩm
+
+    snprintf(buffer, sizeof(buffer), "T2:%.1fC H2:%.0f", dsTemp, capHumidity);
+    u8g2.drawStr(0, 20, buffer); // Dòng 2: Nhiệt độ DS18B20, CO2
+
+    snprintf(buffer, sizeof(buffer), "PM2.5:%.0f PM10:%.0f", pm25, pm10);
+    u8g2.drawStr(0, 30, buffer); // Dòng 3: PM2.5, PM10
+
+    u8g2.sendBuffer(); // Gửi dữ liệu lên OLED
 
     Serial.printf("Nhiệt độ AHT20: %.2f °C, Độ ẩm AHT20: %.2f %%\n", ahtTemp, ahtHumidity);
     Serial.printf("Nhiệt độ DS18B20: %.2f °C, Độ ẩm điện dung: %.2f %%\n", dsTemp, capHumidity);
@@ -207,7 +224,7 @@ void serverReadTask(void *pvParameters)
       {
         String jsonData = response.substring(jsonStart);
 
-        StaticJsonDocument<200> doc; // Buffer 200 byte cho JSON
+        JsonDocument doc; // Buffer 200 byte cho JSON
         DeserializationError error = deserializeJson(doc, jsonData);
 
         if (!error)
@@ -282,6 +299,13 @@ void setup()
   MQ135.setA(110.47);           // Hệ số A cho CO2 (theo datasheet)
   MQ135.setB(-2.862);           // Hệ số B cho CO2 (theo datasheet)
   MQ135.init();                 // Khởi tạo cảm biến
+
+  // Khởi tạo OLED
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB08_tr); // Chọn font chữ nhỏ
+  u8g2.clearBuffer();
+  u8g2.drawStr(0, 10, "Starting...");
+  u8g2.sendBuffer();
 
   dataMutex = xSemaphoreCreateMutex();
 
